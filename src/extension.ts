c1ffea00e16863c402004d43d93a955f9eea2ed1
Marc S. Brooks <marc@mbrooks.info>
2022-08-14 15:44:20 -0700

    Added InputBox prompts, collect values

--- src/extension.ts
+++ src/extension.ts
@@ -10,25 +10,48 @@ export async function activate(context: ExtensionContext) {
   context.subscriptions.push(
     commands.registerCommand('lambda-lambda-lambda.createApp', async () => {
 
-      // Prompt the application name.
-      const appName: string | undefined = await window.showInputBox({
-        placeHolder: 'App name',
+      // Prompt application values.
+      const name: string | undefined = await window.showInputBox({
+        placeHolder: 'Application name',
         validateInput: value => {
-          return (/^[\w-]+$/i.test(value))
-            ? null : 'Alphanumeric characters, no spaces.';
+          return (/^[\w-]{1,40}$/.test(value))
+            ? null : 'Alphanumeric characters, no spaces';
         }
       });
 
+      const description: string | undefined = await window.showInputBox({
+        placeHolder: 'Description',
+        validateInput: value => {
+          return (/^[\w-.,!? ]{1,100}$/.test(value))
+            ? null : 'Alphanumeric characters, .,!? punctuation';
+        }
+      });
+
+      const prefix: string | undefined = await window.showInputBox({
+        placeHolder: 'Prefix',
+        validateInput: value => {
+          return (/^\/[\w-]{1,100}$/.test(value))
+            ? null : 'Alphanumeric characters, must start with /';
+        },
+        value: '/'
+      });
+
+      const timeout: string | undefined = await window.showInputBox({
+        placeHolder: 'Timeout',
+        validateInput: value => {
+          return (/^[\d]{1,2}$/.test(value))
+            ? null : 'Numbers only';
+        },
+        value: '3'
+      });
+
       // Generate sources from templates.
-      if (appName) {
-        const appConfig: AppConfig = {
-          description: 'Example description',
-          name: appName,
-          prefix: '/',
-          timeout: 3
-        };
+      if (description && name && prefix && timeout) {
+        const appConfig: AppConfig = {description, name, prefix, timeout};
 
         createFiles(appConfig, context.extensionPath);
+      } else {
+        window.showErrorMessage('Failed to create application sources.');
       }
     })
   );
