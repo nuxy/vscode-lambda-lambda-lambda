a29f6a87e9ac110e3919e0a045dc25ea45a806aa
Marc S. Brooks <marc@mbrooks.info>
2022-08-15 19:23:03 -0700

    Grouped input into a multi-step format
    Added custom InputBox function

--- src/extension.ts
+++ src/extension.ts
@@ -3,45 +3,64 @@ import {commands, window, ExtensionContext} from 'vscode';
 // Local modules
 import {createFiles, AppConfig} from './generator';
 
+interface InputBoxOpts {
+  placeHolder: string,
+  title: string,
+  validateInput: {(value: string): string | undefined},
+  value?: string,
+  step: number
+}
+
 /**
  * Activate the VS Code extension.
  */
 export async function activate(context: ExtensionContext) {
   context.subscriptions.push(
     commands.registerCommand('lambda-lambda-lambda.createApp', async () => {
+      const inputBoxTitle = (step: number = 1): string => {
+        return `Create new L³ application`;
+      };
 
       // Prompt application values.
-      const name: string | undefined = await window.showInputBox({
+      const name: string | undefined = await promptInputBox({
         placeHolder: 'Application name',
+        title: inputBoxTitle(1),
         validateInput: value => {
-          return (/^[\w-]{1,40}$/.test(value))
-            ? null : 'Alphanumeric characters, no spaces';
-        }
+          return (value && /^[a-zA-Z0-9]{1,40}$/.test(value))
+            ? undefined : 'Alphanumeric characters, no spaces';
+        },
+        step: 1
       });
 
-      const description: string | undefined = await window.showInputBox({
+      const description: string | undefined = await promptInputBox({
         placeHolder: 'Description',
+        title: inputBoxTitle(2),
         validateInput: value => {
-          return (/^[\w-.,!? ]{1,100}$/.test(value))
-            ? null : 'Alphanumeric characters, .,!? punctuation';
-        }
+          return (value && /^[\w-.,!? ]{1,100}$/.test(value))
+            ? undefined : 'Alphanumeric characters, .,!? punctuation';
+        },
+        step: 2
       });
 
-      const prefix: string | undefined = await window.showInputBox({
+      const prefix: string | undefined = await promptInputBox({
         placeHolder: 'Prefix',
+        title: inputBoxTitle(3),
         validateInput: value => {
-          return (/^\/[\w-]{0,100}$/.test(value))
-            ? null : 'Alphanumeric characters, must start with /';
+          return (value && /^\/[\w-]{0,100}$/.test(value))
+            ? undefined : 'Alphanumeric characters, must start with /';
         },
-        value: '/'
+        step: 3,
+        value: '/',
       });
 
-      const timeout: string | undefined = await window.showInputBox({
+      const timeout: string | undefined = await promptInputBox({
         placeHolder: 'Timeout',
+        title: inputBoxTitle(4),
         validateInput: value => {
-          return (/^[\d]{1,2}$/.test(value))
-            ? null : 'Numbers only';
+          return (value && /^[\d]{1,2}$/.test(value))
+            ? undefined : 'Numbers only';
         },
+        step: 4,
         value: '3'
       });
 
@@ -55,4 +74,33 @@ export async function activate(context: ExtensionContext) {
       }
     })
   );
+}
+
+/**
+ * Create a new InputBox instance.
+ */
+async function promptInputBox(opts: InputBoxOpts): Promise<any> {
+  return new Promise((resolve, reject) => {
+    const inputBox = window.createInputBox();
+    inputBox.placeholder = opts.placeHolder;
+    inputBox.title       = opts.title;
+    inputBox.value       = opts.value || '';
+    inputBox.step        = opts.step;
+    inputBox.totalSteps  = 4;
+
+    // Handle events.
+    inputBox.onDidAccept(() => {
+      const value = inputBox.value;
+      const error = opts.validateInput(value);
+
+      if (error) {
+        inputBox.validationMessage = error;
+      } else {
+        inputBox.hide();
+        resolve(value);
+      }
+    });
+
+    inputBox.show();
+  });
 }
