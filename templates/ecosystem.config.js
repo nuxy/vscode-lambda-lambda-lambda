815a5e70293564f4e4b3cf72dc95204396645aa3
Marc S. Brooks <marc@mbrooks.info>
2022-11-04 03:25:13 -0700

    Fixed lint error (double-quotes)

--- templates/ecosystem.config.js
+++ templates/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
-    script: ".devcontainer/server.js",
-    watch: ["{{appBaseDir}}/src"],
+    script: '.devcontainer/server.js',
+    watch: ['{{appBaseDir}}/src'],
     watch_delay: 1000
   }]
-}
+};
