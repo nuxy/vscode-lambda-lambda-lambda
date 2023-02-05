b29564d461284e55be643967eae471b1fc3ae4fa
Marc S. Brooks <marc@mbrooks.info>
2023-02-05 13:35:38 -0800

    Extract info from package.json

--- templates/openapi.js
+++ templates/openapi.js
@@ -1,9 +1,9 @@
 module.exports = {
   openapi: '3.0.0',
   info: {
-    title: '{{appName}}',
-    description: '{{appDescription}}',
-    version: '0.0.1',
+    title: process.env.npm_package_name,
+    description: process.env.npm_package_description,
+    version: process.env.npm_package_version
   },
   servers: [{
     url: 'http://localhost:3000'
